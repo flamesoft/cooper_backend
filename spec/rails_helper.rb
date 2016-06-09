b5a561bdb423239786f1e40597d83df99f2ec8a6
Yan Zhang <emma2740@gmail.com>
2016-06-09 15:38:52 +0200

    Add a helper method that will parse the server response body to JSON

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,11 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+  config.include FactoryGirl::Syntax::Methods
+  config.include ResponseJSON
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
