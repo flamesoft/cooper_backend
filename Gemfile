bde32005a83d85ef131d64a8f489175f2a62b037
Yan Zhang <emma2740@gmail.com>
2016-06-09 14:53:07 +0200

    Add the first test ping

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 #to allow external clients to access our application.
 gem 'rack-cors', require: 'rack/cors'
+gem 'responders', '~> 2.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v0 do
+      resources :ping, only: [:index], constraints: {format: /(json)/}
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
