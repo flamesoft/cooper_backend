7b9e2fcaade838cb9c3871edd04783d5936b5c17
Yan Zhang <emma2740@gmail.com>
2016-06-10 16:53:24 +0200

    Move post related test into a test file under requets folder

--- spec/models/performance_data_spec.rb
+++ spec/models/performance_data_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe PerformanceData, type: :model do
-  let(:user) { FactoryGirl.create(:user) }
-  let(:headers) { {HTTP_ACCEPT: 'application/json'} }
-  let(:credentials) { user.create_new_auth_token }
-
   describe 'Database table' do
     it { is_expected.to have_db_column :id }
     it { is_expected.to have_db_column :data }
@@ -12,17 +8,5 @@ RSpec.describe PerformanceData, type: :model do
 
   describe 'Relations' do
     it { is_expected.to belong_to :user }
-  end
-
-  describe 'Performance Data' do
-    let(:headers){ {HTTP_ACCEPT: 'application/json'}}
-
-    describe 'POST /api/v1/data/' do
-        it 'creates a data entry' do
-          post '/api/v1/data/', {performance_data: {data: {message: 'Average'}}}, headers.merge!(credentials)
-          entry = PerformanceData.last
-          expect(entry.data).to eq({'message'=>'Average'})
-        end
-    end
   end
 end
