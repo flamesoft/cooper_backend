70f09e397db543e36192c898494bd5bdd9f05f5b
Yan Zhang <emma2740@gmail.com>
2016-06-14 14:47:57 +0200

    make the API include authorization credentials in the response headers

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,12 @@ module RailsCooper
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :put, :delete, :post]
+        # resource '*', headers: :any, methods: [:get, :put, :delete, :post]
+        resource '*',
+          headers: :any,
+             methods: [:get, :post, :delete, :put, :options, :head],
+             expose: %w(access-token expiry token-type uid client),
+             max_age: 0
       end
     end
 
